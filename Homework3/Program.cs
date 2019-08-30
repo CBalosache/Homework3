18cfbed7e04f99b483728b96cb76562a2500351d
Cosmin Balosache <cbalosache@spartaglobal.com>
2019-08-30 06:59:10 +0100

    My third homework

---
+++ Homework3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            Console.WriteLine(productAndSum(4,5,6,out sum));
+            Console.WriteLine(sum);
+            Console.Read();
+        }
+        public static double productAndSum(double value1, double value2, double value3, out double sumIn)
+        {
+            sumIn = value1 + value2 + value3;
+            return value1 * value2 * value3;
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat01 = new Cat("Tibby", 2);
+            Cat cat02 = new Cat("Smudge", 1);
+            Cat.Age(cat01);
+            Cat.Age(cat02);
+            var json = new JavaScriptSerializer().Serialize(cat01);
+        }
+    }
+
+    class Cat
+    {
+        private string name;
+        private int age;
+
+        public Cat(string catName, int catAge)
+        {
+            name = catName;
+            age = catAge;
+        }
+
+        public static object Age(Cat Cat)
+        {
+            Cat.age = Cat.age + 5;
+            return Cat.age;
+        }
+    }
+}
